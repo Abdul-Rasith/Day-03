06990fee5bf4468b646d77ca22b2cacda975cac3
Abdul-Rasith <mscrasith@gmail.com>
2024-03-31 11:09:49 +0530

    Upload Day-03 task

--- script3.js
+++ script3.js
@@ -1,16 +1,16 @@
 // use the same rest countries and print all countries 
 // names, regions, sub-region and populations
-var request = new XMLHttpRequest();
+let req = new XMLHttpRequest();
 
-request.open("GET","https://restcountries.com/v3.1/all");
-request.send();
-request.onload = function(){
-    var res = JSON.parse(request.response)
-   console.log(res)
-for (var i=0; i<res.length; i++){
-    console.log(res[i].name.common)
-    console.log(res[i].region+" "+res[i].subregion)
-    console.log(res[i].population)
+req.open("GET","https://restcountries.com/v3.1/all");
+req.send();
+req.onload = function(){
+    let result = JSON.parse(req.response)
+   console.log(result)
+for (var i=0; i<result.length; i++){
+    console.log(result[i].name.common)
+    console.log(result[i].region+" "+result[i].subregion)
+    console.log(result[i].population)
     
 }
 }
